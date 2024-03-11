2ae5344cde3e1c914e1739208e271d6ca2427065
KaranvirB <kbhogal.21@gmail.com>
2024-03-10 23:16:01 -0400

    cleaned up code

--- src/app/api/user/route.tsx
+++ src/app/api/user/route.tsx
@@ -1,18 +1,7 @@
 import prisma from "@/app/lib/db";
-import { NextResponse, NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 import { hash } from "bcrypt"
-import * as z from 'zod';
-import type { NextApiRequest, NextApiResponse } from 'next'
 
-const userSchema = z
-  .object({
-    username: z.string().min(1, 'Username is required').max(100),
-    email: z.string().min(1, 'Email is required').email('Invalid email'),
-    password: z
-      .string()
-      .min(1, 'Password is required')
-      .min(8, 'Password must have more than 8 characters'),
-});
 
 export async function POST(req: Request) {
     if (req.method === 'POST') {
