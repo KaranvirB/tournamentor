45ce279597bee7d977cbaf3470e9a36dd4bd4e0e
KaranvirB <kbhogal.21@gmail.com>
2024-03-13 14:20:04 -0400

    reset repository

--- src/app/providers.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-import { Provider } from "react-redux";
-import { store } from "./store";
-import { ReactNode } from "react";
-
-export function Providers({ children, }: Readonly<{children:ReactNode}> ) {
-  return <Provider store={store}>{children}</Provider>;
-}
